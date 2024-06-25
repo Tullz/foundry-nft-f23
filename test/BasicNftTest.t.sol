c5b57ea9f81ff19394457074a192c4cdedb41d57
Tully <tully.slattery@gmail.com>
2024-06-25 18:46:39 +0800

    added interactions - 8 done

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -9,6 +9,25 @@ import {BasicNft} from "../src/BasicNft.sol";
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("USER");
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
-    function setUp() public {}
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Doggy";
+        string memory actualName = basicNft.name();
+        assertEq(actualName, expectedName);
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+        assert(basicNft.balanceOf(USER) == 1);
+        assertEq(PUG, basicNft.tokenURI(0));
+    }
 }
