86521cd8df3d36bbf9bea58900253869f0d8fd1d
Tully <tully.slattery@gmail.com>
2024-06-28 21:16:19 +0800

    middle of 15

---
+++ script/DeployMoodNft.s.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNft is Script {
+    function run() external returns (MoodNft) {}
+
+    function svgToImageUri(
+        string memory svg
+    ) public pure returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(bytes(string(abi.encodePacked(svg))))
+                )
+            );
+    }
+}
