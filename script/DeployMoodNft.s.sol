8b72d6c24320dff4a33c8a3e3ec6af5e698082bd
Tully <tully.slattery@gmail.com>
2024-06-29 21:36:12 +0800

    15 done

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -8,7 +8,18 @@ import {MoodNft} from "../src/MoodNft.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
-    function run() external returns (MoodNft) {}
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./img/sad.svg");
+        string memory happySvg = vm.readFile("./img/happy.svg");
+
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(
+            svgToImageUri(sadSvg),
+            svgToImageUri(happySvg)
+        );
+        vm.stopBroadcast();
+        return moodNft;
+    }
 
     function svgToImageUri(
         string memory svg
--- test/unit/MoodNftTest.t.sol
+++ test/unit/MoodNftTest.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-import {MoodNft} from "../src/MoodNft.sol";
+import {MoodNft} from "../../src/MoodNft.sol";
 import {Test, console} from "forge-std/Test.sol";
 
 contract MoodNftTest is Test {
